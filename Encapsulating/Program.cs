1311b38fb4aebc449d9df0f097c2d8e13fceb338
unknown <andi.iv@abv.bg>
2017-09-16 17:45:16 +0300

    EncapsulationLab added to github

---
+++ Encapsulating/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var persons = new Team("sfdjhasvf");
+        for (int i = 0; i < lines; i++)
+        {
+            try
+            {
+
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2],CultureInfo.InvariantCulture),
+                    double.Parse(cmdArgs[3]));
+
+                persons.AddPlayers(person);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+
+        Console.WriteLine($"First team have {persons.FirstTeam.Count} players");
+        Console.WriteLine($"Reserve team have {persons.ReserveTeam.Count} players"); 
+
+
+
+    }
+
+}
+
